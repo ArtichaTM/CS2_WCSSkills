7b26f7c3dedac4805b6fb01f43abd3f4108bf528
Articha <tima-1324@mail.ru>
2023-11-06 11:58:52 +0300

    Added status effect tests

---
+++ tests/status_effects/effect_apply.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <unordered_set>
+
+#include "../../src/status_effects/se.hpp"
+
+using namespace std;
+
+TEST(effect_apply, general) {
+	//test_dolili<unordered_set<int>>();
+}
