2c33a5992d44fd27cdba332b618acd44abf31e9d
Articha <tima-1324@mail.ru>
2023-11-13 15:33:48 +0300

    replaced strigns in paths.hpp to filesystem::path renamed DATA_DIR definition to CMAKE_DATA_DIR

--- src/includes/paths.hpp
+++ src/includes/paths.hpp
@@ -2,14 +2,22 @@
 #define WCSSKILLS_PATHS
 
 #include <string>
+#include <filesystem>
 
 
 namespace paths {
-    const static std::basic_string MAIN_DIR = DATA_DIR;
+    using namespace std::filesystem;
+    const static path MAIN_DIR = path(CMAKE_DATA_DIR);
+    const static path DATA_DIR = MAIN_DIR / path("data");
+    const static path SKILLS = DATA_DIR / path("skills.json");
+    const static path SE = DATA_DIR / path("se.json");
+    const static path TRAITS = DATA_DIR / path("traits.json");
+
+    /*const static std::basic_string MAIN_DIR = DATA_DIR;
     const static std::basic_string DATA = MAIN_DIR + R"(\data)";
     const static std::basic_string SKILLS = DATA + R"(\skills.json)";
     const static std::basic_string SE = DATA + R"(\se.json)";
-    const static std::basic_string TRAITS = DATA + R"(\traits.json)";
+    const static std::basic_string TRAITS = DATA + R"(\traits.json)";*/
 }
 
 #endif //WCSSKILLS_PATHS
