40808e98316df558c48186935d0d82b9b798ff0c
Articha <tima-1324@mail.ru>
2023-11-13 15:03:50 +0300

    Made src/includes/paths.hpp take MAIN_DIR variable from preprocessor definitions, which defined in cmake files list

--- src/includes/paths.hpp
+++ src/includes/paths.hpp
@@ -5,7 +5,7 @@
 
 
 namespace paths {
-    const static std::basic_string MAIN_DIR = R"(C:\Users\Articha\Desktop\EveryThing\Editors\Cpp\WCSSkills)";
+    const static std::basic_string MAIN_DIR = DATA_DIR;
     const static std::basic_string DATA = MAIN_DIR + R"(\data)";
     const static std::basic_string SKILLS = DATA + R"(\skills.json)";
     const static std::basic_string SE = DATA + R"(\se.json)";
