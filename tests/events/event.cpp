6e4e9724bb7afb9960a6acb1881047023364fd8c
Articha <tima-1324@mail.ru>
2023-11-23 14:04:12 +0300

    Added event tests

---
+++ tests/events/event.cpp
@@ -0,0 +1,13 @@
+/*
+Tests valid only with DEBUG defined!
+*/
+
+#include <gtest/gtest.h>
+#include "../../src/events/event.hpp"
+#include "../leaks.hpp"
+
+namespace events {
+	TEST(events, general) {
+
+	}
+}
