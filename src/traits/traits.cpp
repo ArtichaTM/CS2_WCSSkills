945fe08339fddf78ff1406d044c5ee114eca23bb
Articha <tima-1324@mail.ru>
2024-01-02 21:33:21 +0300

    Removed Traits class entirely, replaced by unsigned int typedef. Changed "Trait.id" lines

--- src/traits/traits.cpp
+++ src/traits/traits.cpp
@@ -7,27 +7,6 @@ using namespace nlohmann;
 using std::unordered_set;
 
 namespace traits {
-    Trait::Trait(inner_type id) : id(id) {}
-
-    bool Trait::operator==(inner_type _id) const {
-        return this->id == _id;
-    }
-
-    bool Trait::operator==(Trait other) const {
-        return this->id == other.id;
-    }
-
-    bool Trait::operator>(Trait other) const {
-        return this->id > other.id;
-    }
-
-    bool Trait::operator<(Trait other) const {
-        return this->id > other.id;
-    }
-
-    Trait::Trait(std::initializer_list<inner_type>& list) : id(*list.begin()) {}
-
-
     tr_set make(nlohmann::json const& info) {
         tr_set values;
         for (auto& value : info) {
@@ -46,14 +25,11 @@ namespace traits {
     }
 }
 
-std::size_t std::hash<traits::Trait>::operator()(const traits::Trait& s) const noexcept {
-    return s.id;
-}
 
 std::size_t std::hash<traits::tr_set>::operator()(const traits::tr_set &s) const noexcept {
     std::size_t hashcode = 0;
     for (traits::Trait trait: s) {
-        hashcode ^= trait.id;
+        hashcode ^= trait;
     }
     return hashcode;
 }
--- src/traits/traits.hpp
+++ src/traits/traits.hpp
@@ -7,32 +7,19 @@
 
 namespace traits {
 
-    typedef unsigned int inner_type;
-    
-    class Trait {
-    public:
-        const inner_type id;
-        
-        Trait(inner_type);
-        Trait(std::initializer_list<inner_type>&);
-        bool operator==(inner_type) const;
-        bool operator==(Trait) const;
-        bool operator<(Trait) const;
-        bool operator>(Trait) const;
-    };
-    
-    typedef std::unordered_set<traits::Trait> tr_set;
+    typedef unsigned int Trait;
+    namespace {
+        typedef Trait inner_type;
+    }
+
+    typedef std::unordered_set<Trait> tr_set;
     typedef std::unordered_set<tr_set> tr_set_set;
-    typedef std::vector<traits::Trait> tr_vector;
+    typedef std::vector<Trait> tr_vector;
     
     tr_set make(nlohmann::json const&);
-    tr_set make(std::unordered_set<inner_type> const&);
+    tr_set make(std::unordered_set<Trait> const&);
 
 } // namespace traits
-
-template<> struct std::hash<traits::Trait> {
-    std::size_t operator()(traits::Trait const& s) const noexcept;
-};
 
 template<> struct std::hash<traits::tr_set> {
     std::size_t operator()(traits::tr_set const& s) const noexcept;
